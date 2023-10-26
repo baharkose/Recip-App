2902e1e3d2cf85f5478193ab7c5894b707afb047
baharkose <baharkse17@gmail.com>
2023-10-26 16:21:31 +0300

    added ingredients

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const inputDiv = document.querySelector(".search-text"); 
+const inputDiv = document.querySelector(".search-text");
 const searchBtn = document.querySelector(".search-btn");
 const cardImg = document.querySelector(".food-img");
 const imgContent = document.querySelector(".food-sub-title");
+const ingredientsList = document.querySelector(".ingredients");
+const recipeBtn = document.querySelector(".view-recipe-btn");
+const instructions = document.querySelector(".instructionsX");
+const exitBtn = document.querySelector(".fa-times");
+const strInstructions = document.querySelector(".strInstructions");
 
+
+
+// strInstructions
 
 
 
@@ -10,11 +18,50 @@ const imgContent = document.querySelector(".food-sub-title");
 async function recipe() {
     const api = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputDiv.value}`);
 
-    const response = await api.json(); 
+    const response = await api.json();
     console.log(response);
+
+    // Yemek resmini ve adını güncelle
     cardImg.src = response.meals[0].strMealThumb;
-    imgContent.innerHTML = response.meals[0].strArea;
+    imgContent.innerHTML = response.meals[0].strMeal;
+
+    // Malzemeleri görüntüle
+    displayIngredients(response.meals[0]);
+
+
+
 }
 
 searchBtn.addEventListener("click", recipe);
 
+function displayIngredients(meal) {
+    // Önce mevcut içeriği temizle
+    ingredientsList.innerHTML = "";
+
+    // Malzemeleri listele
+    for (let i = 1; i <= 20; i++) {
+        const ingredientKey = `strIngredient${i}`;
+        const measureKey = `strMeasure${i}`;
+
+        // Malzeme ve ölçüsü boş değilse ekle
+        if (meal[ingredientKey] && meal[ingredientKey].trim() !== "") {
+            const ingredientItem = document.createElement("li");
+            ingredientItem.textContent = `${meal[measureKey]} ${meal[ingredientKey]}`;
+            ingredientsList.appendChild(ingredientItem);
+        }
+    }
+}
+
+
+function viewRecipe(){
+    console.log("aaa");
+    instructions.style.display = "block";
+    strInstructions.innerHTML = response.meals[0].strMeal;
+
+}
+
+
+function invisibleIns(){
+    instructions.style.display = "none";
+
+}
